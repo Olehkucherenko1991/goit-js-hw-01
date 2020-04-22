48066f42b65e70c06b18b26e1bd800f1d91f7092
Olehkucherenko1991 <kucherenko.oleh91@gamil.com>
2020-04-22 20:23:39 +0300

    hw-01-js-fix

--- task-2.js
+++ task-2.js
@@ -1,4 +1,10 @@
 const total = 100;
 const ordered = 50;
-const isBuying = ordered >= 1 && ordered <= total;
-console.log(`Заказ оформлен, с вами свяжется менеджер`, isBuying);
+let message;
+let isBuying = ordered >= 1 && ordered <= total;
+if ((isBuying = ordered > total)) {
+  message = 'На складе недостаточно твоаров!';
+} else if ((isBuying = ordered >= 1 && ordered <= total)) {
+  message = 'Заказ оформлен, с вами свяжется менеджер';
+}
+console.log(message);
--- task-3.js
+++ task-3.js
@@ -4,13 +4,11 @@ const userPassword = prompt('Введите пароль!');
 if (ADMIN_PASSWORD === userPassword) {
   let message = 'Добро пожаловать!';
   alert(message);
-  console.log(message);
 } else if (userPassword === null) {
   let message = 'Отменено пользователем!';
   alert(message);
-  console.log(message);
 } else {
   let message = 'Доступ запрещен, неверный пароль!';
   alert(message);
-  console.log(message);
 }
+console.log(message);
